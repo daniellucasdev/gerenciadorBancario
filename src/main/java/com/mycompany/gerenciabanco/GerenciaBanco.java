dba7f1d165a1ee8ce57f16cea778a0843eaa9530
Dan💀🔥 <daniel.ferreira2675@gmail.com>
2025-10-29 09:48:50 -0300

    fix: :fire: Cadastro inicial de cliente

--- src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
+++ src/main/java/com/mycompany/gerenciabanco/GerenciaBanco.java
@@ -1,6 +1,28 @@
 package com.mycompany.gerenciabanco;
 
+import java.util.Scanner;
 
 public class GerenciaBanco {
 
+    static class ContaBancaria {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== sistema de gerenciamento bancário ===");
+        System.out.println("Iniciando cadastro\n");
+
+        System.out.print("Informe seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.print("Informe seu sobrenome: ");
+        String sobrenome = sc.nextLine();
+
+        System.out.print("Informe seu CPF: ");
+        String cpf = sc.nextLine();
+
+        System.out.println("\nCadastro concluído com sucesso");
+    }
 }
